255eb1d3772a4612038740248432cb1fb7794296
abdopcnet <abdopcnet@gmail.com>
2026-01-02 20:42:11 +0000

    🆕

---
+++ lms_custom/lms_custom/doctype/website_settings/test_website_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, abdopcnet@gmail.om and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestWebsiteSettings(FrappeTestCase):
+	pass
